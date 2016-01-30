07da060c9056ac2ad06185a515390877c21ea771
y850830 <40343236@gm.nfu.edu.tw>
2016-01-30 14:40:27 +0800

    find db

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,11 +16,26 @@ Route::get('/', function () {
 });
 
 Route::get('orm', function () {
-    App\Post::create([
-    	'title'=>'title',
-    	'content'=>'content',
-    	'is_feature' =>false,    
-    ]);
+    // App\Post::create([
+    // 	'title'=>'title',
+    // 	'content'=>'content',
+    // 	'is_feature' =>false,    
+    // ]);
+
+	//$post = new \App\Post;
+    // $post->title ='ntitle';
+    // $post->content = 'ncontent';
+    // $post->is_feature = false;
+    // $post->save();
+
+    // $posts =\App\Post::all();
+    // dd($posts);
+
+	// $posts = \App\Post::find(1);
+	// dd($posts);
+
+	$posts = \App\Post::where('id','<',10)->orwhere('title','=','title')->get();
+	dd($posts);
 });
 
 /*
